286c1fb6792f3387db45bf806b60271560155132
KGiamou <k.giabou@gmail.com>
2021-01-22 14:54:53 +0100

    Update functions

--- functions.r
+++ functions.r
@@ -1,3 +1,59 @@
+columnmean  <- function(y, removeNA = TRUE) {
+  nc <- ncol(y)
+  means <- numeric(nc)
+  for (i in 1:nc)
+  {
+    means[i] <- mean(y[,i], na.rm = removeNA)
+  }
+  means  
+}
+
+pollutantmean <- function(folder, pm, id){
+  
+  setwd(paste("C:/Users/kgiab/Desktop/DataScience/rprog_data_specdata/", folder, sep=""))
+  
+  files <- list.files(pattern = ".csv")
+  means <- as.vector(NULL)
+  
+  
+  for (i in id){
+    
+    monitor <- read.csv(files[i], h = TRUE, sep=",")
+    me <- mean(monitor[, pm], na.rm = TRUE)
+    means <- c(means, me)
+    
+  }
+  print(means)
+  
+}
+pollutantmean(folder="specdata", "sulfate", 1:10)
+pollutantmean(folder="specdata", "nitrate", 73:99)
+
+library(gtools)
+
+complete <- function(folder, id){
+  setwd(paste("C:/Users/kgiab/Desktop/DataScience/rprog_data_specdata/", folder, sep=""))
+  
+  files <- list.files(pattern = ".csv")
+  
+  for (i in id){
+    
+    monitor <- read.csv(files[i], h = TRUE, sep=",")
+    comple <- complete.cases(monitor)
+    data <- data.frame(id=i, nobs=sum(comple))
+    assign(paste("data_", i, sep=""), data)
+    
+  }
+  ll <- mixedsort(ls(pattern="data_"))
+  ll_1 <- lapply(ll, get)
+  comp <- do.call(rbind, ll_1)
+  print(comp)
+  rm(list = ls(pattern="data_"))
+  
+}
+
+complete(folder = "specdata", id = 30:25)
+
 thres <- function(folder, threshold=0){
   setwd(paste0("C:/Users/KSGI/Desktop/Data Science/specdata/", folder, sep=""))
   
