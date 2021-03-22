3583527c23bd9bda6446e4a92e61fc00233fb406
KSGI (Kostis Giampoudakis) <ksgi@novonordisk.com>
2021-03-22 18:32:04 +0100

    update assignment

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-library(tidyr)
+library(tidyverse)
 
 setwd("./Getting and Cleaning Data/")
 
@@ -11,22 +11,40 @@ data <- download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfi
                       method = "curl")
 
 
-data2<- unzip("./Course Assignment/wearable.zip", exdir = "./Course Assignment/Assignment data")
+data2<- unzip("./Course Assignment/wearable.zip", files=NULL, exdir = "./Course Assignment")
 
-setwd("./Course Assignment/Assignment data/UCI HAR Dataset")
+setwd("./Course Assignment/UCI HAR Dataset")
 
-Xtrain <- as_tibble(read.table("./train/X_train.txt"))
-ytrain <- read.table("./train/y_train.txt")
-subj_tr <- read.table("./train/subject_train.txt")
+X_train <- as_tibble(read.table("./train/X_train.txt"))
+y_train <- read.table("./train/y_train.txt")
+s_train <- read.table("./train/subject_train.txt") %>%
+           rename(Subjects=V1)
 
-Xtest <- as_tibble(read.table("./test/X_test.txt"))
-ytest <- read.table("./test/y_test.txt")
-subj_test <- read.table("./test/subject_test.txt")
+X_test <- as_tibble(read.table("./test/X_test.txt"))
+y_test <- read.table("./test/y_test.txt")
+s_test <- read.table("./test/subject_test.txt") %>%
+          rename(Subjects=V1)
 
-activ <- read.table("activity_labels.txt")
 
-Xdata <- rbind(Xtrain, Xtest) 
-ydata <- rbind(ytrain, ytest)
+activity <- read.table("activity_labels.txt") %>%
+            rename(Activity=V2)
 
-featur <- read.table("features.txt")
-f_info <- read.table("features_info.txt")
+feature <- read.table("features.txt") %>%
+           rename(Features=V2)
+
+colnames(X_train)  <- as.vector(featur$Features)
+colnames(X_test)  <- as.vector(featur$Features)
+
+
+Atrain <- cbind(s_train, y_train, X_train) 
+Atest  <- cbind(s_test, y_test,X_test)
+
+Atrain2 <- merge(activ, Atrain, by.x="V1", by.y="V1", all=FALSE) %>%
+           select(-V1)
+
+Atest <- 
+
+  
+
+
+View(Xdata)
